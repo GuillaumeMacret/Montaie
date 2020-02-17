0f31b46db9d2b4e000fd7d09ed4f8ec3d9c90976
Guillaume Macret <guillaume.macret@gmail.com>
2020-02-17 11:18:30 +0100

    Lasers for ai

---
+++ Assets/Scripts/AIs/EnemyControler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControler : MonoBehaviour
+{
+    public Transform LaserShoot,LaserShootPosition;
+    public AudioSource LaserAudioSource;
+
+    public float fireCooldown = .5f;
+	private float lastFireCooldownCpt = 0f;
+
+    void Update() {
+		if(lastFireCooldownCpt > 0)lastFireCooldownCpt -= Time.deltaTime;
+		if (Input.GetButton("Fire1") && lastFireCooldownCpt <= 0)
+		{
+			lastFireCooldownCpt = fireCooldown;
+			GameObject.Instantiate(LaserShoot, LaserShootPosition.position, LaserShootPosition.rotation);
+			if (LaserAudioSource != null)
+				LaserAudioSource.Play();
+		}
+	}
+}
