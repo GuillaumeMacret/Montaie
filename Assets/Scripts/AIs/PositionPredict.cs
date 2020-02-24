7d826703c1c21ea4971aee43a3187e19244b1b88
Guillaume Macret <guillaume.macret@gmail.com>
2020-02-24 08:56:21 +0100

    :sparkles: Movement prediction

---
+++ Assets/Scripts/AIs/PositionPredict.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionPredict : MonoBehaviour
+{
+    Transform PredictedLocation;
+    public Rigidbody ObjectToPredictRigidBody;
+
+    public int memorySize = 10;
+    Vector3[] LastVelocities;
+    private int m_LastVelocitiesIndex = 0;
+
+    private Vector3 rememberPredict;
+    private void Start()
+    {
+        PredictedLocation = GetComponent<Transform>();
+        LastVelocities = new Vector3[memorySize];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 velocity = ObjectToPredictRigidBody.velocity;
+
+        Vector3 predict = ObjectToPredictRigidBody.position + velocity;
+
+        rememberPredict += predict;
+        rememberPredict /= 2;
+
+        Debug.Log(rememberPredict);
+        PredictedLocation.transform.position = rememberPredict;
+    }
+}
