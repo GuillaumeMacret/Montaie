c56ed4db48b7fc7af1bbea3436463f937d79ffae
Kairozen <geoffbaudelet@yahoo.fr>
2020-03-10 01:22:53 +0100

    :construction: Occlusion portals working with triggers

---
+++ Assets/Scripts/OcclusionPortalTrigger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OcclusionPortalTrigger : MonoBehaviour
+{
+	private MeshRenderer meshRenderer;
+	private OcclusionPortal occlusionPortal;
+    
+	// Start is called before the first frame update
+    void Start()
+    {
+		meshRenderer = GetComponent<MeshRenderer>();
+		meshRenderer.enabled = false;
+		occlusionPortal = GetComponent<OcclusionPortal>();
+		occlusionPortal.open = true;
+    }
+
+	private void OnTriggerEnter(Collider other) {
+		if(other.gameObject.tag == "Player") {
+			Debug.Log("Closed");
+			occlusionPortal.open = false;
+		}
+	}
+
+	private void OnTriggerExit(Collider other) {
+		if (other.gameObject.tag == "Player") {
+			Debug.Log("Opened");
+			occlusionPortal.open = true;
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+        
+    }
+}
